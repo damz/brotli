87f82ff0487efec05920fd20b12e07f2383423d7
Damien Tournoud <damien@platform.sh>
2018-07-13 09:57:34 +0200

    Add -lm

---
+++ cgo.go
@@ -0,0 +1,11 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Distributed under MIT license.
+// See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+
+package brotli
+
+// Inform golang build system that it should link brotli libraries.
+
+// #cgo LDFLAGS: -lm
+import "C"
